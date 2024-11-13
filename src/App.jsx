26b4f03c62a6c8dfd2284a22385a48bad6af92e9
Thusikaran <pushparasa.thusikaran@gmail.com>
2024-11-13 23:23:31 +0530

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import { Route, Routes } from 'react-router-dom'
 import ProjectDetails from './pages/ProjectDetails/ProjectDetails'
 import IssueDetails from './pages/IssueDetails/IssueDetails'
 import Subscription from './pages/Subscription/Subscription'
+import Auth from './pages/Auth/Auth'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    {false?
+    <div>
     <Navbar/>
      <Routes>
         <Route path='/' element={<Home/>} />
@@ -19,6 +22,10 @@ function App() {
         <Route path='/project/:projectId/issue/:issueId' element={<IssueDetails/>} />
         <Route path='/upgrade_plan' element={<Subscription/>} />
      </Routes>
+    </div>
+    :<Auth/>
+     }
+   
     </>
   )
 }
